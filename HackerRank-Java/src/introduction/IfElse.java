b91120da2f82394c1648c625f43bfc8c092543b5
Rukiye <rukiyeeziz@gmail.com>
2020-08-05 23:14:59 -0400

    worked on if/else and output formatting examples

---
+++ HackerRank-Java/src/introduction/IfElse.java
@@ -0,0 +1,46 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class IfElse {
+	private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    	System.out.println("Input: ");
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if ( (N % 2) == 0 && ( N >= 2 && N <= 5) )
+            System.out.println("Not Weird");
+        else if ( (N % 2) == 0 && (N >= 6 && N <= 20) )
+            System.out.println("Weird");
+        else if ( (N % 2) == 0 && (N > 20) )
+            System.out.println("Not Weird");
+        else
+           System.out.println("Weird"); 
+
+        scanner.close();
+    }
+
+}
+
+/*
+ * Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+ */
